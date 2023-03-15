3e343f382fca10608d82762649ed44c596aa9a98
Victoriia Miroshnikova <victoriia.pl@gmail.com>
2023-03-15 22:07:44 +0200

    внесено правки

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -4,5 +4,9 @@ export default async function fetchCountries(countryname) {
   const response = await fetch(
     `https://restcountries.com/v3.1/name/${countryname}?fields=name,capital,population,flags,languages`
   );
+  if (!response.ok) {
+    Notify.info('Oops, there is no country with that name');
+    return;
+  }
   return await response.json();
 }
